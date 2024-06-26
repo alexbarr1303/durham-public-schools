c6ee28ba9eb1cf45880eb356a19c2e921d2767a0
alexbarr1303 <156235418+alexbarr1303@users.noreply.github.com>
2024-06-26 14:19:12 +0000

    Created first two dataloader functions

--- src/lib/dataloader_alex.py
+++ src/lib/dataloader_alex.py
@@ -1,12 +1,15 @@
-PATH_PARCELS = r"/path"
-PATH_DU_EST = r"/path"
+import pandas as pd
+import geopandas as gpd
+
+PATH_PARCELS = r"../data/extracted_parcels/Parcels_NEW.shp"
+PATH_DU_EST = r"../data/parcels_cleanup.csv"
 
 
 def get_parcels():
-    result_df = None
+    result_df = pd.read_file("../data/extracted_parcels/Parcels_NEW.shp")
     return result_df
 
 
 def get_du_est():
-    result_df = None
+    result_df = pd.read_csv("../data/parcels_cleanup.csv")
     return result_df
